6c680b69157415e209221c2acb7e3f506edefaa1
Lncvrt <lncvrtreal@gmail.com>
2025-07-15 12:49:47 -0700

    Remove Travertine

--- minecraft/papermc/index.php
+++ minecraft/papermc/index.php
@@ -3,7 +3,7 @@ ob_start();
 ?>
 <h1>PaperMC software downloads</h1>
 <p>Select what project by PaperMC you want to get downloads for</p>
-<p><a href="/minecraft/papermc/paper/">Paper</a> &bull; <a href="/minecraft/papermc/travertine/">Travertine</a> &bull; <a href="/minecraft/papermc/waterfall/">Waterfall</a> &bull; <a href="/minecraft/papermc/velocity/">Velocity</a> &bull; <a href="/minecraft/papermc/folia/">Folia</a></p>
+<p><a href="/minecraft/papermc/paper/">Paper</a> &bull; <a href="/minecraft/papermc/waterfall/">Waterfall</a> &bull; <a href="/minecraft/papermc/velocity/">Velocity</a> &bull; <a href="/minecraft/papermc/folia/">Folia</a></p>
 <?php
 $content = ob_get_clean();
 $topic = "PaperMC downloads";
